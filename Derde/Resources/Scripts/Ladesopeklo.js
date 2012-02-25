42daffff9add19b957b0ca63ea6a7cae219ef788
unknown <ladik@.(none)>
2012-02-25 22:10:05 +0100

    derde - doufam ze uz beta ...
    - smer doleva v galerii az na 2. klik

--- Derde/Resources/Scripts/Ladesopeklo.js
+++ Derde/Resources/Scripts/Ladesopeklo.js
@@ -69,31 +69,34 @@ var Settings = function (options) {
 
 	};
 };
-	
-var Cache = function() {
-		var c = new Array();
 
-		var contains = function(key) {
-			if (!c) return false;
-			return c[key] != null;
-		};
+var Cache = function () {
+	var c = new Array();
 
-		return {
-			get: function(key) {
-				if (contains(key)) {
-					return c[key];
-				}
-			},
+	var contains = function (key) {
+		if (!c) return false;
+		return c[key] != null;
+	};
 
-			set: function(key, value) {
-				c[key] = value;
-			},
-
-			has: function(key) {
-				return contains(key);
+	return {
+		reset: function () {
+			$.clean(c);
+		},
+		get: function (key) {
+			if (contains(key)) {
+				return c[key];
 			}
+		},
 
-		};
+		set: function (key, value) {
+			c[key] = value;
+		},
+
+		has: function (key) {
+			return contains(key);
+		}
+
+	};
 };
 
 var url = {
@@ -150,9 +153,9 @@ var galerycontentHelper = {
 
 };
 
-var Loader = function (cache, settings) {
+var Loader = function (cache, settings, callbacks) {
 	var c = new Cache();
-
+	var self = this;
 	var template = function (url) {
 		if (cache)
 			if (c.has(url)) {
@@ -162,11 +165,15 @@ var Loader = function (cache, settings) {
 		var ret = $.ajax({
 			url: url,
 			async: false,
-			cache: cache,
-			success: function (data) {
+			//cache: cache,
+			contentType: "application/xml",
+			//setRequestHeader("Content-Type", "application/xml"),
+			success: function (data, x, h) {
 				if (cache) c.set(url, data);
+
 			}
 		});
+		//ret.header("content-type:application/xml;charset=utf-8");
 		return ret.responseText;
 	};
 
@@ -190,54 +197,90 @@ var Loader = function (cache, settings) {
 		return null;
 	};
 	var contentMain = $("#contentMain");
-	var contentIntro = $("#contentIntro");
-	var displayIntro = function (t) {
-		contentMain.hide();
-		contentIntro.fadeIn();
-	};
+
 	var displayContent = function (t) {
-		contentIntro.hide();
-		contentMain.show();
-		if (t) {
-			contentMain.html(t);
-		}
+		contentMain.html(t);
 	};
 
+	//getne "count" novinek, veme vsechny pro count =0 
+	var getNovinkyList = function (count) {
+		var list = jsonData(null, "Novinky", "Service/listCulture.php");
+		var len = list.length > count ? count : list.length;
+		var ret = new Array();
+		for (var i = 0; i < len; i++) {
+			var item = getnovinka(list[i]);
+			item.linkname = list[i];
+			ret.push(item);
+		}
+
+		return ret;
+
+	};
+
+	var loading = function (elem) {
+		if (!elem) {
+			elem = contentMain;
+		}
+		elem.html("<p>loading..</p>");
+	};
 	var getnovinka = function (name) {
 		var nka = template("Content/Novinky/" + name + "." + settings.culture() + ".html");
+
 		var ret = {
-			header: $(nka).find("header").html(),
+			date: $(nka).find("date").text(),
+			header: $(nka).find("header").text(),
 			contentheader: $(nka).find("contentheader").html(),
 			contentmain: $(nka).find("contentmain").html()
 		};
+
 		return ret;
 	};
 
 	return {
+		resetCache: function () {
+			c.reset();
+		},
 		culture: function () {
 			var a = "#" + settings.culture();
 			$(a).closest(".language").find(".selected").removeClass("selected");
 			$(a).addClass("selected");
 		},
 		home: function () {
-			displayIntro();
+			loading();
+			var t = $.tmpl($("#template_home"), { data: getNovinkyList(3) });
+			displayContent(t);
+			callbacks.homeCallback(
+			//handler pro reseni prepinani novinek v intru 
+				function (name) {
+					var context = $(".introboxcontent");
+					var nov = getnovinka(name);
+
+					context.find(".intrheader").stop(false, true).fadeOut(100, function () {
+						$(this).html(nov.header).fadeIn("slow");
+					});
+					context.find(".intrcontent").stop(false, true).fadeOut(100, function () {
+						$(this).html(nov.contentheader).fadeIn(1000);
+					});
+					return false;
+				}
+			);
+
 			url.selected(null, true);
 		},
 		content: function (action) {
+			loading();
 			var t = template("Content/" + action + "." + settings.culture() + ".html");
 			displayContent(t);
+			if (action == "kontakt") {
+				callbacks.kontaktCallback();
+			}
 			url.selected();
-			$("#mcs_container").mCustomScrollbar("vertical", 260, "easeOutCirc", 1.05, "auto", "yes", "yes", 10);
 		},
 		novinky: function () {
-			displayContent();
-			contentMain.html("loading..");
+			loading();
 			var t = template("Content/template_novinkylistItem.html");
-
-
 			var list = jsonData(null, "Novinky", "Service/listCulture.php");
-			console.log(list);
-			
+
 			contentMain.html("");
 			$.each(list, function (i, value) {
 				$.tmpl(t, getnovinka(value)).appendTo(contentMain);
@@ -247,25 +290,19 @@ var Loader = function (cache, settings) {
 			url.selected(null, true);
 
 		},
-		novinka: function (name) {
 
-			var t = template("Content/template_novinky.html");
-			var xxx = $.tmpl(t, getnovinka(name));
-
-			displayContent(xxx);
+		novinkyThumbs: function () {
+			return getNovinkyList(4);
 		},
 
-		intronovinka: function (name, context) {
-			var nov = getnovinka(name);
-
-			context.find(".intrheader").stop(false, true).fadeOut(100, function () {
-				$(this).html(nov.header).fadeIn("slow");
-			});
-			context.find(".intrcontent").stop(false, true).fadeOut(100, function () {
-				$(this).html(nov.contentheader).fadeIn(1000);
-			});
-			return false;
+		novinka: function (name) {
+			loading();
+			var t = template("Content/template_novinky.html");
+			var xxx = $.tmpl(t, getnovinka(name));
+			displayContent(xxx);
+			url.selected(null);
 		}
+
 
 	};
 
@@ -316,6 +353,11 @@ var Core = function (settings, loader) {
 				loader.culture(a);
 				refreshPage();
 				break;
+			case "novinkyThumbs":
+				return loader.novinkyThumbs(a);
+			case "reset":
+				loader.resetCache();
+				break;
 			default:
 				loader.home(a);
 				saveState(c, a);
@@ -327,6 +369,7 @@ var Core = function (settings, loader) {
 	var ccc;
 	var aaa;
 	function loadContent() {
+		//console.log(settings.controller(), settings.action());
 		load(settings.controller(), settings.action());
 		ccc = settings.controller();
 		aaa = settings.action();
@@ -339,6 +382,23 @@ var Core = function (settings, loader) {
 			$.address.change(function (e) {
 				loadContent();
 			});
+		},
+		Load: function (controller, action) {
+			load(controller, action);
+		},
+		LoadTo: function (controller, action, destination, template) {
+
+			destination.html("<p>Loading...</p>");
+			var c = load(controller, action);
+
+			if ($(template).length > 0) {
+				var jqTemplate = $(template).template('jqTemplate');
+				var html = $.tmpl(jqTemplate, { data: c });
+				destination.html(html);
+
+				return;
+			}
+			destination.html(c);
 		}
 	};
 

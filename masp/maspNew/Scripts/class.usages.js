32378dce5af999088364d6905636964d9dbca080
vladimir talas <vladimir.talas@gmail.com>
2013-02-25 12:26:02 +0100

    kostickovac

--- masp/maspNew/Scripts/class.usages.js
+++ masp/maspNew/Scripts/class.usages.js
@@ -1,31 +1,63 @@
 /*global JSLINQ*/
+var Position = (function () {
+	"use strict";
+
+	function Position(x1, y1, x2, y2) {
+		this.x1 = x1 || 0;
+		this.y1 = y1 || 0;
+		this.x2 = x2 || 0;
+		this.y2 = y2 || 0;
+	}
+
+	Position.prototype.toString = function () {
+		return this.x1 + "," + this.y1 + "," + this.x2 + "," + this.y2;
+	};
+
+	return Position;
+}());
+
+var SquareItem = (function () {
+	"use strict";
+
+	function SquareItem(width, height) {
+		this.position = new Position();
+		this.width = width;
+		this.height = height;
+	}
+
+	return SquareItem;
+}());
+
 var Usages = (function () {
+	"use strict";
 
 	function Usages(width, height) {
 		this.usages = [];
 		this.maxWidth = width || 0;
 		this.maxHeight = height;
+		this.widthOffset = 0;
+		this.newLineOffset = 0;
+		this.newLineOffsetTop = 0;
+		this.heightOffset = 0;
 	}
 
-	Usages.prototype.findFirstEmptyVertical = function (x, squareItemToPut) {
-		return new Position();
+	Usages.prototype.setRandom = function (widthOffset, newLineOffset,heightOffset, newLineOffsetTop) {
+		this.widthOffset = widthOffset;
+		this.newLineOffset = newLineOffset;
+		this.heightOffset = heightOffset;
+		this.newLineOffsetTop = newLineOffsetTop;
 	};
 
-	Usages.prototype.mostBottomOn = function (x) {
-		var mostBottom = new SquareItem();
 
-		JSLINQ(this.usages).Select(function (item) {
-			if (x >= item.position.x1 && x <= item.position.x2 && item.position.y2 >= mostBottom.position.y2) {
-				mostBottom = item;
-			}
-		});
+	Usages.prototype.generate = function (items, newwidth) {
+		if (newwidth) {
+			this.maxWidth = newwidth;
+		}
 
-		return mostBottom;
-	};
-
-	Usages.prototype.generate = function (items) {
 		var i = 0,
 			len = items.length;
+
+		this.usages = [];
 
 		for (i; i < len; i++) {
 			this.addSquare(items[i]);
@@ -58,12 +90,11 @@ var Usages = (function () {
 		}
 	};
 
-
 	Usages.prototype.findMostBottom = function (x1, x2) {
-		var max = new SquareItem();
-		var x = this.itemsInInterval(x1, x2);
+		var max = new SquareItem(),
+			x = this.itemsInInterval(x1, x2);
 
-		JSLINQ(x).Where(function (item) {
+		new JSLINQ(x).Where(function (item) {
 			var pos = item.position,
 				isMoreBellow = pos.y2 > max.position.y2;
 
@@ -76,25 +107,42 @@ var Usages = (function () {
 	};
 
 	Usages.prototype.itemsInInterval = function (x1, x2) {
-		var inInterval = JSLINQ(this.usages).Where(function (item) {
+		var inInterval = new JSLINQ(this.usages).Where(function (item) {
 			var pos = item.position;
-			return pos.x1 >= x1 && pos.x1 <= x2;
+			return (pos.x1 > x1 && pos.x1 < x2) || (pos.x1 <= x1 && pos.x2 >= x2) || (pos.x2 > x1 && pos.x2 < x2);
 		});
 		return inInterval.items;
 	};
 
 	Usages.prototype.addSquare = function (squareItem) {
 		var last = this.getLastItemPosition(),
+			newLineOffsetRandom,
+			newLineOffsetTopRandom,
+			heightOffsetRandom,
+			widthOffsetRandom,
 			newPosition = new Position();
 
-//		if (last.x2 + squareItem.width > this.maxWidth) {
-			newPosition.x1 = last.x2 + squareItem.width > this.maxWidth ? 0 : last.x2;
-			newPosition.x2 = newPosition.x1 + squareItem.width;
-			newPosition.y1 = this.findMostBottom(newPosition.x1, newPosition.x2).position.y2;
-			newPosition.y2 = squareItem.height + newPosition.y1;
-//		} else {
-//			newPosition = new Position(last.x2, last.y1, last.x2 + squareItem.width, last.y1 + squareItem.height);
-//		}
+
+		newLineOffsetRandom = this.newLineOffset ? Math.floor((Math.random()*this.newLineOffset) + 1) : 0;
+		newLineOffsetTopRandom = this.newLineOffsetTop ? Math.floor((Math.random()*this.newLineOffsetTop) + 1) : 0;
+		widthOffsetRandom = this.widthOffset ? Math.floor((Math.random()*this.widthOffset) + 1) : 2;
+		heightOffsetRandom = this.heightOffset ? Math.floor((Math.random()*this.heightOffset) + 1) : 2;
+
+
+		newPosition.x1 = last.x2 + squareItem.width > this.maxWidth ? 0  : last.x2 + widthOffsetRandom;
+		if (newPosition.x1 === 0 || last.x2 === 0) {
+			newPosition.x1 += newLineOffsetRandom;
+		}
+
+		newPosition.x2 = newPosition.x1 + squareItem.width;
+		newPosition.y1 = this.findMostBottom(newPosition.x1, newPosition.x2).position.y2 + heightOffsetRandom;
+		if (newPosition.y1 - heightOffsetRandom === 0) {
+			newPosition.y1 += newLineOffsetTopRandom;
+		}
+
+
+		newPosition.y2 = squareItem.height + newPosition.y1;
+
 		squareItem.position = newPosition;
 
 		this.addUsage(squareItem);
@@ -110,44 +158,5 @@ var Usages = (function () {
 	};
 
 	return Usages;
-})
-	();
+}());
 
-var SquareItem = (function () {
-
-	function SquareItem(width, height) {
-		this.position = new Position();
-		this.width = width;
-		this.height = height;
-	}
-
-	return SquareItem;
-})();
-
-var Point = (function () {
-	function Point(x, y) {
-		this.x = x || 0;
-		this.y = y || 0;
-	}
-
-	Point.prototype.toString = function () {
-		return this.x + "," + this.y;
-	};
-	return Point;
-})();
-
-var Position = (function () {
-
-	function Position(x1, y1, x2, y2) {
-		this.x1 = x1 || 0;
-		this.y1 = y1 || 0;
-		this.x2 = x2 || 0;
-		this.y2 = y2 || 0;
-	}
-
-	Position.prototype.toString = function () {
-		return this.x1 + "," + this.y1 + "," + this.x2 + "," + this.y2;
-	};
-
-	return Position;
-})();
--- masp/maspNew/Scripts/controller.x.js
+++ masp/maspNew/Scripts/controller.x.js
@@ -1,50 +1,39 @@
 /*global Usages*/
 function xController($scope, galleryApi, menuApi) {
 
-	var boxes = [
-		new SquareItem(100, 200),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(200, 100),
-		new SquareItem(200, 100),
-		new SquareItem(200, 100),
-		new SquareItem(200, 100),
-		new SquareItem(100, 100),
-		new SquareItem(60, 200),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 200),
-		new SquareItem(100, 100),
-		new SquareItem(50, 200),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100),
-//		new SquareItem(100, 100)
-	];
+	var boxes = [],
+		usages = new Usages($scope.width);
 
-	var x = new Usages(900);
+	$scope.width = 900;
 
-	$scope.data = x.generate(boxes);
+	$scope.widthOffset = 0;
+	$scope.newLineOffset = 0;
+	$scope.heightOffset = 0;
+	$scope.newLineOffsetTop = 0;
+
+	for (var i =0 ; i< 50; i++){
+		var x = Math.floor((Math.random()*100)+30);
+		boxes.push( new SquareItem(x,x));
+	}
+
+	$scope.data = usages.generate(boxes, $scope.width);
+
+	$scope.aaa = function (newWidth){
+		//$scope.width = newWidth;
+		$scope.data = usages.generate(boxes, newWidth);
+	};
+
+	$scope.setRandom = function (){
+		usages.setRandom($scope.widthOffset, $scope.newLineOffset, $scope.heightOffset, $scope.newLineOffsetTop);
+		usages.generate(boxes);
+	};
 
 	$scope.getWidth = function (item) {
 		return item.position.x2 - item.position.x1;
 	};
+
 	$scope.getHeight = function (item) {
 		return item.position.y2 - item.position.y1;
 	};
 
-	console.log($scope.data);
 }
